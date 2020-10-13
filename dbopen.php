01076a72a1eba918111c5135ec65fbaaeeb07323
Tuure <tuure.vairio@gmail.com>
2020-10-13 09:50:06 +0300

    tried to get dpopen.php to work

--- dbopen.php
+++ dbopen.php
@@ -7,7 +7,7 @@
  //$dbname = 'opisk_t3vatu01';
  $username = 't3vatu01';
  $password = '';
- $con = mysqli_connect($host,$username,$password,); //$dbname
+ $con = mysqli_connect($host,$username,$password); //$dbname
 
  if (mysqli_connect_errno()) {
  echo "Failed to connect to mysli database: " . mysqli_connect_error();
